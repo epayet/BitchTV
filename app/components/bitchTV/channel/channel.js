afb965591e10c167f2f6b7f24d4cff0be4929450
epayet <jakspinning@gmail.com>
2015-02-03 17:57:23 +0100

    Loading

--- app/components/bitchTV/channel/channel.js
+++ app/components/bitchTV/channel/channel.js
@@ -1,12 +1,13 @@
-angular.module('BitchTV').directive('bitchTvChannel', function (ChannelsResource, ChannelService) {
+angular.module('BitchTV').directive('bitchTvChannel', function (ChannelsResource, ChannelService, WaitingService) {
     return {
         restrict: 'E',
         templateUrl: 'components/bitchTV/channel/channel.html',
         link: function(scope, element, attrs) {
             var channelId = attrs.channelId;
-            //scope.channel = ChannelsResource.get({id: channelId});
+            WaitingService.start();
             ChannelService.get(channelId, function(channel) {
                 scope.channel = channel;
+                WaitingService.stop();
             });
             scope.getChannelLogoSrc = ChannelService.getChannelLogoSrc;
         }
